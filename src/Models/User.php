25ea131f18581ddce43b031ffcb561fe55f942b3
Vlad06013 <vlaprokudi06013@gmail.com>
2024-08-16 16:23:30 +0700

    add admin

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'users';
-
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'permissions',
+    ];
     public function userTgSettings()
     {
         return $this->hasMany('Valibool\TelegramConstruct\Models\UserTgSettings', 'user_id', 'id');
